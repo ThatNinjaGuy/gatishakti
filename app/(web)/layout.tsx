90fd6c85d266432b372da5ef6edcc6521f157c3b
ThatNinjaGuy <vishalgautam.dtu@gmail.com>
2024-02-26 00:16:18 +0530

    Resolving folder structure for build issues

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -4,12 +4,12 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-import Footer from "../components/footer/Footer";
+import Footer from "../components/Footer/Footer";
 import ThemeProvider from "../components/ThemeProvider/ThemeProvider";
 import { NextAuthProvider } from "../components/AuthProvider/AuthProvider";
 import Toast from "../components/Toast/Toast";
-import Header from "../components/Header/Header";
 import { ProductCountProvider } from "../context/ProductCountContext";
+import Header from "../components/Header/Header";
 
 // Setting up font for the complete global website
 const poppins = Poppins({
